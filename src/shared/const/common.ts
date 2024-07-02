a05c1c4e5d759a94ff0306c2144439262f384d39
Xamana <gamingtest@mail.ru>
2024-07-02 15:53:13 +0300

    add some changed

--- src/shared/const/common.ts
+++ src/shared/const/common.ts
@@ -1,13 +1,13 @@
 export enum Currency {
-    "RUB" = "RUB",
-    "USD" = "USD",
-    "EUR" = "EUR"
+    'RUB' = 'RUB',
+    'USD' = 'USD',
+    'EUR' = 'EUR'
 }
 
 export enum Country {
-    Russia = "Russia",
-    Belarus = "Belarus",
-    Ukraine = "Ukraine",
-    Kazakhstan = "Kazakhstan",
-    Armenia = "Armenia",
+    Russia = 'Russia',
+    Belarus = 'Belarus',
+    Ukraine = 'Ukraine',
+    Kazakhstan = 'Kazakhstan',
+    Armenia = 'Armenia',
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -14,9 +14,8 @@ export default (env: BuildEnv) => {
     const mode = env.mode || 'development';
     const PORT = env.port || 3000;
     const apiUrl = env.apiUrl || 'https://localhost:8000';
-    
+
     const isDev = mode === 'development';
-    
 
     const config: webpack.Configuration = buildWebpackConfig({
         mode,
